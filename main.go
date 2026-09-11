5560e53dc3b07638364fdf66d7c1e1d27f93d700
Poet <zhenhaowang@dimleap.ai>
2026-09-11 17:29:31 +0800

    feat: 初始化

---
+++ main.go
@@ -0,0 +1,9 @@
+// Package main tells Go this file belongs to an executable program.
+// Go starts running the program from the main function in this package.
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Println("Hello, World!")
+}
